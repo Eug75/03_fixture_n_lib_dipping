952052f788c37b713ebbada626bfbdb074f8ab46
Evgeny Amelchenko <amelch@yandex.ru>
2025-05-24 14:13:05 +0300

    добавил фикстуру размера окна браузера

--- test_from_task01.py
+++ test_from_task01.py
@@ -1,9 +1,12 @@
 from selene import browser, be, have
 import pytest
 
-def test_search_task01():
+@pytest.fixture
+def browser_window_size():
     browser.config.window_width = 1200
     browser.config.window_height = 900
+
+def test_search_task01(browser_window_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
     assert browser.element('html').should(have.text('Об этой странице'))
