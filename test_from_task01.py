36940da776abe9c2699ddc4a24cf7a008ae6f6a2
Evgeny Amelchenko <amelch@yandex.ru>
2025-05-24 15:09:39 +0300

    добавил тест на "ничего не найдено"

--- test_from_task01.py
+++ test_from_task01.py
@@ -1,9 +1,14 @@
-import pytest
+#import pytest
+
 from selene import browser, be, have
 
 def test_search_task01(browser_window_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
     assert browser.element('html').should(have.text('Об этой странице'))
-
     # browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_search_nothing(browser_window_size):
+    browser.open('https://duckduckgo.com')
+    browser.element('[name="q"]').should(be.blank).type('kjbkjsadrfeg').press_enter()
+    assert browser.element('html').should(have.text('ничего не найдено'))
