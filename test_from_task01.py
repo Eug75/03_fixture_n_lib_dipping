ddee62cddd7faaf389ff982e0a1979f5bf9691e7
Evgeny Amelchenko <amelch@yandex.ru>
2025-05-24 13:12:35 +0300

    переписал скрипт из первого задания через функцию pytest, но что-то как-то корявенько, т.к. "import pytest" серый...

---
+++ test_from_task01.py
@@ -0,0 +1,10 @@
+from selene import browser, be, have
+import pytest
+
+def test_search_task01():
+
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
+    assert browser.element('html').should(have.text('Об этой странице'))
+
+    # browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
